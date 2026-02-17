ddc39d979095098afbe5a0af7f807f6e06983820
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2026-02-17 10:45:36 -0700

    Favorite API endpoint implemented

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,49 @@
 import express from "express";
 import "dotenv/config";
-import { ENV } from "./config/env.js";
+import {ENV} from "./config/env.js";
+import {db} from "./config/db.js";
+import {favoritesTable} from "./db/schema.js";
 
 const app = express();
+//to get the request from the body
+app.use(express.json());
+
 const PORT = ENV.PORT || 8001;
 
+
 app.get("/api/health", (req, res) => {
-  res.status(200).json({ success: true });
+    res.status(200).json({success: true});
 });
 
+
+app.post("/api/favorites", async (req, res) => {
+    try {
+        const {userId, recipeId, title, image, cookTime, servings} = req.body;
+        if (!userId || !recipeId || !title) {
+            return res.status(400).json({error: "Missing required fields"});
+        }
+
+
+        const newFavorite = await db.insert(favoritesTable).values({
+            userId,
+            recipeId,
+            title,
+            image,
+            cookTime,
+            servings
+        }).returning() // to save it and in returning() if you don't pass anything it's going to return everything
+
+        res.status(200).json(newFavorite[0]);
+
+    } catch (error) {
+        console.log("Error adding favorite", error)
+        res.status(500).json({error: error.message});
+    }
+})
+
+
 app.listen(PORT, () => {
-  console.log(`Server is running on PORT: ${PORT} `);
+    console.log(`Server is running on PORT: ${PORT} `);
 });
+
+
